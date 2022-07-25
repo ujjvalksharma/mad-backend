37145f72ac8cbb9f3146285f4906172edfa3c2d9
ujjvalksharma <ujjvalksharma@gmail.com>
2022-07-24 21:53:44 -0700

    auth service created

---
+++ src/main/java/com/mad/MadBackendApplication.java
@@ -0,0 +1,12 @@
+package com.mad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mad/MadBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
