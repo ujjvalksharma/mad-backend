b675b762540ecc143039b527b1c6737cd18be061
ujjvalksharma <ujjvalksharma@gmail.com>
2022-07-31 14:57:23 -0700

    Implement service to send push notification

--- src/main/java/com/mad/dto/Notification.java
+++ src/main/java/com/mad/dto/Notification.java
@@ -1,25 +1,21 @@
 package com.mad.dto;
 
-public class Notification {
+import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Getter
+public class Notification implements Serializable{
 	
-	class notification {
-		String title;
-		String body;
-	  }
+	private String token;
+	private String body;
+	private String title;
 }
-
-
-/*
-
-{
-
-"notification":{
-	"title": "demo title",
-	"body": "demo body"
-},
-"to":"es7R8B0_RHacxdqn6Dga0B:APA91bGzovFTHQdeMeCzND6voRopnaTvBBWMwbStXTYs96m6RkInRw8ODeiBCUcnA17T7uCA566IhhnwaJGyoj88nMYRbYp67diMVqc1nr1xqIFNIkrfbfe1i95r2-tdXeiIWNoDGsXb"
-
-}
-
-*/
